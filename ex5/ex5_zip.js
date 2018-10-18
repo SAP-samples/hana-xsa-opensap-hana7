9e49550cfade09d3689b346cdb5a6a9a4088b892
Thomas Jung <thomas.jung@sap.com>
2018-10-18 09:54:09 -0400

    Update ex5_zip.js

--- ex5/ex5_zip.js
+++ ex5/ex5_zip.js
@@ -8,63 +8,52 @@ module.exports = function() {
 
 	//Hello Router
 	app.get("/", (req, res) => {
-		var output = `<H1>ZIP Examples</H1></br> 
-			<a href="${req.baseUrl}/example1">/example1</a> - Download data in ZIP format - folder and files</br>` +
-			require(global.__base + "utils/exampleTOC").fill();
+		var output =
+			`<H1>ZIP Examples</H1></br> 
+			<a href="${req.baseUrl}/example1">/example1</a> - Download data in ZIP format - folder and files</br>
+			<a href="${req.baseUrl}/zipPO">/zipPO</a> - Download Purchase Orders in ZIP format</br>`;
 		res.type("text/html").status(200).send(output);
 	});
 
 	//Simple Database Select - In-line Callbacks
 	app.get("/example1", (req, res) => {
-
-		var zip = new require("node-zip")();
+		let zip = new require("node-zip")();
 		zip.file("folder1/demo1.txt", "This is the new ZIP Processing in Node.js");
 		zip.file("demo2.txt", "This is also the new ZIP Processing in Node.js");
-		var data = zip.generate({
+		let data = zip.generate({
 			base64: false,
 			compression: "DEFLATE"
 		});
-
 		res.header("Content-Disposition", "attachment; filename=ZipExample.zip");
-		res.type("application/zip").status(200).send(new Buffer(data, "binary"));
-
+		return res.type("application/zip").status(200).send(new Buffer(data, "binary"));
 	});
 
 	//Simple Database Select - In-line Callbacks
-	app.get("/zipPO", (req, res) => {
-		var client = req.db;
-		var query =
-			"SELECT TOP 25000 \"PurchaseOrderId\", \"PartnerId\", \"CompanyName\", \"CreatedByLoginName\", \"CreatedAt\", \"GrossAmount\" " +
-			"FROM \"PO.HeaderView\" order by \"PurchaseOrderId\" ";
-		client.prepare(
-			query,
-			(err, statement) => {
-				if (err) {
-					res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
-					return;
-				}
-				statement.exec([],
-					(err, rs) => {
-						if (err) {
-							res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
-						} else {
-							var out = "";
-							for (let item of rs){
-								out += item.PurchaseOrderId + "\t" + item.PartnerId + "\t" + item.CompanyName + "\t" + item.CreatedByLoginName + "\t" + item.CreatedAt + "\t" + item.GrossAmount + "\n";
-							}
-
-							var zip = new require("node-zip")();
-							zip.file("po.txt", out);
-							var data = zip.generate({
-								base64: false,
-								compression: "DEFLATE"
-							});
-
-							res.header("Content-Disposition", "attachment; filename=poWorklist.zip");
-							res.type("application/zip").status(200).send(new Buffer(data, "binary"));
-						}
-					});
+	app.get("/zipPO", async(req, res) => {
+		try {
+			const dbClass = require(global.__base + "utils/dbPromises");
+			let client = new dbClass(req.db);
+			const statement = await client.preparePromisified(
+				`SELECT TOP 25000 "PurchaseOrderId", "PartnerId", "CompanyName", "CreatedByLoginName", "CreatedAt", "GrossAmount" 
+				   FROM "PO.HeaderView" ORDER BY "PurchaseOrderId" `
+			);
+			const results = await client.statementExecPromisified(statement, []);
+			let out = "";
+			for (let item of results) {
+				out += item.PurchaseOrderId + "\t" + item.PartnerId + "\t" + item.CompanyName + "\t" + item.CreatedByLoginName + "\t" + item.CreatedAt +
+					"\t" + item.GrossAmount + "\n";
+			}
+			let zip = new require("node-zip")();
+			zip.file("po.txt", out);
+			let data = zip.generate({
+				base64: false,
+				compression: "DEFLATE"
 			});
+			res.header("Content-Disposition", "attachment; filename=poWorklist.zip");
+			return res.type("application/zip").status(200).send(new Buffer(data, "binary"));
+		} catch (e) {
+			return res.type("text/plain").status(500).send(`ERROR: ${e.toString()}`);
+		}
 	});
 
 	return app;
