18157447dd829eb2c5e85f84b13ef522b9fdaf03
Thomas Jung <thomas.jung@sap.com>
2018-10-18 14:21:07 -0400

    Update read1.js

--- ex5/read1.js
+++ ex5/read1.js
@@ -1,3 +1,17 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-undef:0, no-process-exit:0, new-cap:0*/
+/*eslint-env node, es6 */
+
+"use strict";
+
+const uuid = require("uuid/v4");
+const cds = require("@sap/cds");
+
+
+module.exports = function (entities) {
+	const {
+		catalog
+	} = entities;
+
 	this.after("READ", entities.POItems, (entity) => {
 		if (entity.length > 0) {
 			let now = new Date();
@@ -6,3 +20,6 @@
 			entity[0].DELIVERYDATE = nextMonth.toJSON();
 		}
 	});
+
+
+};	
