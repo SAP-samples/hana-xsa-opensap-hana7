2f50ad3b5083301256528e9a4c1a1dd3547288dd
Thomas Jung <thomas.jung@sap.com>
2018-10-18 09:51:18 -0400

    Update ex5_xml.js

--- ex5/ex5_xml.js
+++ ex5/ex5_xml.js
@@ -6,34 +6,31 @@ var XmlDocument = require("xmldoc").XmlDocument;
 
 module.exports = function() {
 	var app = express.Router();
-
-
 	//Hello Router
 	app.get("/", (req, res) => {
-			var output = `<H1>XML Examples</H1></br>
-				<a href="${req.baseUrl}/example1">/example1</a> - Simple XML parsing</br>` +
-				require(global.__base + "utils/exampleTOC").fill();
-			res.type("text/html").status(200).send(output);
-		});
+		var output = `<H1>XML Examples</H1></br>
+				<a href="${req.baseUrl}/example1">/example1</a> - Simple XML parsing</br>`;
+		res.type("text/html").status(200).send(output);
+	});
 
 	//Simple Database Select - In-line Callbacks
 	app.get("/example1", (req, res) => {
-			var xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
-				'<!-- this is a note -->\n' +
-				'<note noteName="NoteName">' +
-				'<to>To</to>' +
-				'<from>From</from>' +
-				'<heading>Note heading</heading>' +
-				'<body>Note body</body>' +
-				'</note>';
-			var body = "";
-			var note = new XmlDocument(xml);
-			note.eachChild((item) => {
-				body += item.val + '</br>';
-			});
-			res.type("text/html").status(200).send(body);
-
+		const xml =
+		`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+			<!-- this is a note -->
+			<note noteName="NoteName">
+				<to>To</to>
+				<from>From</from>
+				<heading>Note heading</heading>
+				<body>Note body</body>
+			</note>
+		</xml>`;
+		let body = "";
+		let note = new XmlDocument(xml);
+		note.eachChild((item) => {
+			body += item.val + '</br>';
 		});
-
+		return res.type("text/html").status(200).send(body);
+	});
 	return app;
 };
