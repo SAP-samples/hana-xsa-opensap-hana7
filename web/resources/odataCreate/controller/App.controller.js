51e75109dd26f0c0d22063d73088d250190fc4b8
jungsap <thomas.jung@sap.com>
2019-01-28 16:50:14 -0500

    UI5 Evo -> odataCreate

--- web/resources/odataCreate/controller/App.controller.js
+++ web/resources/odataCreate/controller/App.controller.js
@@ -1,14 +1,15 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-use-before-define: 0, no-redeclare: 0, no-undef: 0*/
+/*eslint-env es6 */
 //To use a javascript controller its name must end with .controller.js
 sap.ui.define([
 	"opensap/odataTest/controller/BaseController",
 	"sap/ui/model/json/JSONModel"
-], function(BaseController, JSONModel) {
+], function (BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("opensap.odataTest.controller.App", {
 
-		onInit: function() {
+		onInit: function () {
 
 			var oConfig = this.getOwnerComponent().getModel("config");
 			var userName = oConfig.getProperty("/UserName");
@@ -18,7 +19,7 @@ sap.ui.define([
 			this.getOwnerComponent().getModel().setProperty("/mEntity1", "purchaseDetails");
 
 		},
-		callService: function() {
+		callService: function () {
 			var oTable = this.getView().byId("tblPOHeader");
 
 			var mPath = this.getOwnerComponent().getModel().getProperty("/mPath");
@@ -42,21 +43,14 @@ sap.ui.define([
 				oTable.setInitiallyVisibleFields(headerFields);
 			}
 
-			oModel.attachMetadataLoaded(oModel, function() {
+			oModel.attachMetadataLoaded(oModel, function () {
 				fnLoadMetadata();
 			});
 
-			oModel.attachMetadataFailed(oModel, function() {
-				sap.m.MessageBox.show("Bad Service Definition", {
-					icon: sap.m.MessageBox.Icon.ERROR,
-					title: "Service Call Error",
-					actions: [sap.m.MessageBox.Action.OK],
-					styleClass: "sapUiSizeCompact"
-				});
-			});
+			oModel.attachMetadataFailed(oModel, oDataFailed);
 		},
-		
-		callCreate: function(oEvent) {
+
+		callCreate: function (oEvent) {
 			var oTable = this.getView().byId("tblPOHeader");
 			var oModel = oTable.getModel();
 			//	var result = this.getView().getModel().getData();
@@ -76,32 +70,14 @@ sap.ui.define([
 				"content-type": "application/json;charset=utf-8"
 			});
 			var mParams = {};
-			mParams.success = function() {
-				sap.m.MessageToast.show("Create successful");
+			mParams.success = function () {
+				sap.ui.require(["sap/m/MessageToast"], (MessageToast) => {
+					MessageToast.show("Create successful");
+				});
 			};
-			mParams.error = this.onErrorCall;
+			mParams.error = onODataError;
 			var mEntity1 = this.getOwnerComponent().getModel().getProperty("/mEntity1");
-			oModel.create("/"+mEntity1, oEntry, mParams);
-		},
-
-		onErrorCall: function(oError) {
-			if (oError.statusCode === 500 || oError.statusCode === 400 || oError.statusCode === "500" || oError.statusCode === "400") {
-				var errorRes = JSON.parse(oError.responseText);
-				if (!errorRes.error.innererror) {
-					sap.m.MessageBox.alert(errorRes.error.message.value);
-				} else {
-					if (!errorRes.error.innererror.message) {
-						sap.m.MessageBox.alert(errorRes.error.innererror.toString());
-					} else {
-						sap.m.MessageBox.alert(errorRes.error.innererror.message);
-					}
-				}
-				return;
-			} else {
-				sap.m.MessageBox.alert(oError.response.statusText);
-				return;
-			}
-
+			oModel.create("/" + mEntity1, oEntry, mParams);
 		}
 	});
 });
