3072bc79f9c5545ee2e88de9beed75f312da9fa4
Thomas Jung <thomas.jung@sap.com>
2019-06-28 18:57:43 -0400

    Update db11.js

--- ex5/db11.js
+++ ex5/db11.js
@@ -1,6 +1,6 @@
-	app.get("/org", (req, res) => {
+	app.get("/cfApi", (req, res) => {
 		let VCAP = JSON.parse(process.env.VCAP_APPLICATION);
-		return res.type("application/json").status(200).send(JSON.stringify(VCAP.organization_name));
+		return res.type("application/json").status(200).send(JSON.stringify(VCAP.cf_api));
 	});
 
 	app.get("/space", (req, res) => {
