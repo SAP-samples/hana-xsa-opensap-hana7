d3e5d1172feda858d41da4b1a5b6689057389080
Jung <thomas.jung@sap.com>
2019-08-08 14:10:58 -0400

    Adjust port for local testing

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,7 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-undef:0, no-process-exit:0*/
 /*eslint-env node, es6 */
 "use strict";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 const server = require("http").createServer();
 
 const cds = require("@sap/cds");
